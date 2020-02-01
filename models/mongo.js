e99f1089ec4cd12856414b8fcd3a4d07a5773141
sohadQtaitat <qtaitat3@gmail.com>
2020-02-01 17:04:14 +0200

    create base file

---
+++ models/mongo.js
@@ -0,0 +1,38 @@
+'use strict';
+
+class Model {
+
+
+  constructor() {
+    this.schema = this.schema;
+    console.log('this.schema', this.schema);
+  }
+
+
+  get(_id) {
+    if (_id) {
+      return this.schema.findOne({ _id });
+    }
+    else {
+      return this.schema.find({});
+    }
+  }
+
+
+  post(record) {
+    let newRecord = new this.schema(record);
+    console.log('the newrecord :', newRecord);
+    return newRecord.save();
+  }
+
+  update(_id, record) {
+    return this.schema.findByIdAndModify(_id, record);
+  }
+
+  delete(_id) {
+    return this.schema.findByIdAndDelete(_id);
+  }
+
+}
+
+module.exports = Model;
---
+++ models/products/products.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Model = require('../mongo.js');
+const schema = require('./products-schema.js');
+
+class Products extends Model {
+  constructor (){
+    super();
+    this.schema = schema;
+  }
+
+}
+
+module.exports = Products;
